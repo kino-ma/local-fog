32d7d02837b038af7cbd73b3cb868b2e8c6e9a94
kino-ma <ma@kino.ma>
2023-01-07 19:06:29 +0900

    feat: add logging to handlers

--- node.go
+++ node.go
@@ -1,9 +1,23 @@
 package main
 
+import "fmt"
+
 type Node struct {
 }
 
-func (n Node) HandlePing(p Ping)             {}
-func (n Node) HandleSync(s Sync)             {}
-func (n Node) HandleCall(c Call)             {}
-func (n Node) HandleGetProgram(g GetProgram) {}
+func (n Node) HandlePing(p Ping) {
+	fmt.Printf("ping: %+v", p)
+}
+
+func (n Node) HandleSync(s Sync) {
+	fmt.Printf("sync: %+v", s)
+
+}
+
+func (n Node) HandleCall(c Call) {
+	fmt.Printf("call: %+v", c)
+}
+
+func (n Node) HandleGetProgram(g GetProgram) {
+	fmt.Printf("get program: %+v", g)
+}
