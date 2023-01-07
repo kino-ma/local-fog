5512eeb9ddd4e17ab5d766b6777fc3cd7a730565
kino-ma <ma@kino.ma>
2023-01-07 16:52:49 +0900

    feat: add tcp server

--- server.go
+++ server.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const DEFAULT_HOST = "0.0.0.0"
+const DEFAULT_PORT = "46866"
+const CONNECTION_TYPE = "tcp"
+
+var logger = log.New(os.Stderr, "", 0)
+
 type Server interface {
+	Handle(conn net.Conn)
+}
+
+func Listen(s Server, host string, port int) (err error) {
+	addr := DEFAULT_HOST + ":" + DEFAULT_PORT
+	l, err := net.Listen(CONNECTION_TYPE, addr)
+
+	if err != nil {
+		err = fmt.Errorf("failed to linten on address %v: %e", addr, err)
+		return err
+	}
+
+	defer l.Close()
+	logger.Printf("Start listening on '%v'...\n", addr)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			err = fmt.Errorf("failed to accept connection: %e", err)
+			return err
+		}
+
+		go s.Handle(conn)
+	}
 }
