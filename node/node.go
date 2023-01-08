919560302edc75ec654aa2278534d6b259f8383a
kino-ma <ma@kino.ma>
2023-01-08 17:12:04 +0900

    fix: change handlers to recevie pointers

--- node/node.go
+++ node/node.go
@@ -9,19 +9,19 @@ import (
 type Node struct {
 }
 
-func (n Node) HandlePing(p t.Ping) {
+func (n Node) HandlePing(p *t.Ping) {
 	fmt.Printf("ping: %+v\n", p)
 }
 
-func (n Node) HandleSync(s t.Sync) {
+func (n Node) HandleSync(s *t.Sync) {
 	fmt.Printf("sync: %+v\n", s)
 
 }
 
-func (n Node) HandleCall(c t.Call) {
+func (n Node) HandleCall(c *t.Call) {
 	fmt.Printf("call: %+v\n", c)
 }
 
-func (n Node) HandleGetProgram(g t.GetProgram) {
+func (n Node) HandleGetProgram(g *t.GetProgram) {
 	fmt.Printf("get program: %+v\n", g)
 }
