79b52be42d12a8ddc9f84f488b1e9a3c5ef93cf5
kino-ma <ma@kino.ma>
2023-01-09 17:07:55 +0900

    feat: add client `Connect` function

--- core/fogdevice.go
+++ core/fogdevice.go
@@ -1,4 +1,30 @@
 package core
 
-type FogDevice interface {
+import (
+	"fmt"
+
+	pb "local-fog/core/types"
+
+	"google.golang.org/grpc"
+)
+
+type FogConsumer struct {
+	Conn   *grpc.ClientConn
+	Client pb.LocalFogClient
+}
+
+func Connect(host string, port int) (FogConsumer, error) {
+	target := host + ":" + fmt.Sprint(port)
+	conn, err := grpc.Dial(target)
+
+	if err != nil {
+		return FogConsumer{}, err
+	}
+
+	client := pb.NewLocalFogClient(conn)
+
+	return FogConsumer{
+		Conn:   conn,
+		Client: client,
+	}, nil
 }
