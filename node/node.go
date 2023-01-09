79b3d4f48e23992427c84d9329a259992db4791d
kino-ma <ma@kino.ma>
2023-01-09 17:21:47 +0900

    fix: request logging

--- node/node.go
+++ node/node.go
@@ -12,25 +12,25 @@ type Node struct {
 }
 
 func (n *Node) Ping(ctx context.Context, p *t.PingRequest) (*t.PingReply, error) {
-	fmt.Printf("ping: %+v\n", p)
+	fmt.Printf("ping\n")
 
 	return &t.PingReply{}, nil
 }
 
 func (n *Node) Sync(ctx context.Context, p *t.SyncRequest) (*t.SyncReply, error) {
-	fmt.Printf("ping: %+v\n", p)
+	fmt.Printf("sync\n")
 
 	return &t.SyncReply{}, nil
 }
 
 func (n *Node) Call(ctx context.Context, p *t.CallRequest) (*t.CallReply, error) {
-	fmt.Printf("ping: %+v\n", p)
+	fmt.Printf("call: id = %v, body = %v\n", p.AppId, p.Body)
 
 	return &t.CallReply{}, nil
 }
 
 func (n *Node) GetProgram(ctx context.Context, p *t.GetProgramRequest) (*t.GetProgramReply, error) {
-	fmt.Printf("ping: %+v\n", p)
+	fmt.Printf("getProgram: id = %v\n", p.AppId)
 
 	return &t.GetProgramReply{}, nil
 }
