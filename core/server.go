24b54ccee5ac3bae45ee305717f464821b6a6a27
kino-ma <ma@kino.ma>
2023-01-09 16:35:30 +0900

    feat: use grpc server

--- core/server.go
+++ core/server.go
@@ -2,14 +2,13 @@ package core
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
 
 	t "local-fog/core/types"
 
-	"google.golang.org/protobuf/proto"
+	"google.golang.org/grpc"
 )
 
 const DEFAULT_HOST = "0.0.0.0"
@@ -18,14 +17,7 @@ const CONNECTION_TYPE = "tcp"
 
 var logger = log.New(os.Stderr, "", 0)
 
-type Server interface {
-	HandlePing(p *t.Ping)
-	HandleSync(s *t.Sync)
-	HandleCall(c *t.Call)
-	HandleGetProgram(g *t.GetProgram)
-}
-
-func Listen(s Server, host string, port int) (err error) {
+func Listen(s t.LocalFogServer, host string, port int) (err error) {
 	addr := host + ":" + fmt.Sprint(port)
 	l, err := net.Listen(CONNECTION_TYPE, addr)
 
@@ -34,83 +26,10 @@ func Listen(s Server, host string, port int) (err error) {
 		return err
 	}
 
+	gs := grpc.NewServer()
+	t.RegisterLocalFogServer(gs, s)
+
 	logger.Printf("Start listening on '%v'...\n", addr)
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			err = fmt.Errorf("failed to accept connection: %e", err)
-			return err
-		}
-
-		handle(&s, conn)
-	}
-}
-
-func handle(s *Server, conn net.Conn) {
-	defer conn.Close()
-	ss := *s
-
-	req := &t.Request{}
-	buf, err := ioutil.ReadAll(conn)
-	if err != nil {
-		logger.Printf("Failed to read all from conn: %e\n", err)
-		return
-	}
-
-	err = proto.Unmarshal(buf, req)
-	if err != nil {
-		logger.Printf("Failed to unmarshal request: %e\n", err)
-		return
-	}
-
-	switch req.RequestType {
-	case t.Request_PING:
-		p := req.GetPing()
-
-		if p == nil {
-			typeName := t.Request_Type_name[int32(req.RequestType)]
-			content := req.GetConetnt()
-			logger.Printf("Requet type and content mismatch: type '%v (%v)' for %+v", typeName, req.RequestType, content)
-			return
-		}
-
-		ss.HandlePing(p)
-
-	case t.TYPE_SYNC:
-		s := req.GetSync()
-
-		if s == nil {
-			typeName := t.Request_Type_name[int32(req.RequestType)]
-			content := req.GetConetnt()
-			logger.Printf("Requet type and content mismatch: type '%v (%v)' for %+v", typeName, req.RequestType, content)
-			return
-		}
-
-		ss.HandleSync(s)
-
-	case t.TYPE_CALL:
-		c := req.GetCall()
-
-		if c == nil {
-			typeName := t.Request_Type_name[int32(req.RequestType)]
-			content := req.GetConetnt()
-			logger.Printf("Requet type and content mismatch: type '%v' for %+v", typeName, content)
-			return
-		}
-
-		ss.HandleCall(c)
-
-	case t.TYPE_GET_PROGRAM:
-		g := req.GetGetProgram()
-
-		if g == nil {
-			typeName := t.Request_Type_name[int32(req.RequestType)]
-			content := req.GetConetnt()
-			logger.Printf("Requet type and content mismatch: type '%v' for %+v", typeName, content)
-			return
-		}
-
-		ss.HandleGetProgram(g)
-	}
+	return gs.Serve(l)
 }
