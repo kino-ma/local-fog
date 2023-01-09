56351a2ec4a991f4a4e301806d39dcb4cbad2874
kino-ma <ma@kino.ma>
2023-01-09 17:13:25 +0900

    feat: add reqeust wrapper funcitons

--- core/fogdevice.go
+++ core/fogdevice.go
@@ -1,7 +1,9 @@
 package core
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	pb "local-fog/core/types"
 
@@ -27,4 +29,34 @@ func Connect(host string, port int) (FogConsumer, error) {
 		Conn:   conn,
 		Client: client,
 	}, nil
+}
+
+func (c FogConsumer) Ping(req *pb.PingRequest) (*pb.PingReply, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	return c.Client.Ping(ctx, req)
+}
+
+func (c FogConsumer) Sync(req *pb.SyncRequest) (*pb.SyncReply, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	return c.Client.Sync(ctx, req)
+}
+
+func (c FogConsumer) Call(req *pb.CallRequest) (*pb.CallReply, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	return c.Client.Call(ctx, req)
+
+}
+
+func (c FogConsumer) GetProgram(req *pb.GetProgramRequest) (*pb.GetProgramReply, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	return c.Client.GetProgram(ctx, req)
+
 }
