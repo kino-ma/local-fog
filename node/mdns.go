ac1b37b4b5ba575315d9494dad12d78eaa6e4478
kino-ma <ma@kino.ma>
2023-01-10 20:10:56 +0900

    feat: use hashicorp/mdns version

--- node/mdns.go
+++ node/mdns.go
@@ -3,32 +3,30 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 
 	"github.com/hashicorp/mdns"
-	"golang.org/x/net/ipv4"
 )
 
-var GetMdnsCann chan *mdns.Conn = make(chan *mdns.Conn)
-
 func RegisterAndServeMdns() error {
-	addr, err := net.ResolveUDPAddr("udp", mdns.DefaultAddress)
+	host, err := os.Hostname()
 	if err != nil {
-		return fmt.Errorf("failed to resolve udp address: %v", err)
+		return fmt.Errorf("failed to get the hostname: %v", err)
 	}
 
-	l, err := net.ListenUDP("udp4", addr)
+	info := []string{"LocalFog"}
+	ips := []net.IP{net.IPv4(127, 0, 0, 1)}
+	service, err := mdns.NewMDNSService(host, "_localfog._tcp", "local", "", 46866, ips, info)
+
 	if err != nil {
-		return fmt.Errorf("failed to listen udp: %v", err)
+		return fmt.Errorf("failed ot create ne wmDNS service: %v", err)
 	}
 
-	conn, err := mdns.Server(ipv4.NewPacketConn(l), &mdns.Config{
-		LocalNames: []string{"_localfog._tcp.local"},
-	})
+	server, err := mdns.NewServer(&mdns.Config{Zone: service})
 	if err != nil {
-		close(GetMdnsCann)
-		return fmt.Errorf("failed to create mdns server: %v", err)
+		return fmt.Errorf("failed to create new mdns server: %v", err)
 	}
-	GetMdnsCann <- conn
+	defer server.Shutdown()
 
-	select {}
+	return nil
 }
