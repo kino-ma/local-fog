ca85f11b4792408b37bf17ebfb8bb1267fd98260
kino-ma <ma@kino.ma>
2023-01-12 11:30:44 +0900

    test: ouput request failure log

--- client/main.go
+++ client/main.go
@@ -8,15 +8,38 @@ import (
 
 func main() {
 	addr := Discover()
-	log.Println(addr)
+	log.Printf("discovered: %v", addr)
 	consumer, err := core.Connect(addr.String(), core.DEFAULT_PORT)
 
 	if err != nil {
-		log.Fatalf("failed to connec to the server: %e", err)
+		log.Fatalf("failed to connec to the server: %v", err)
 	}
 
-	consumer.Ping(&types.PingRequest{})
-	consumer.Sync(&types.SyncRequest{})
-	consumer.Call(&types.CallRequest{})
-	consumer.GetProgram(&types.GetProgramRequest{})
+	pr, err := consumer.Ping(&types.PingRequest{})
+	if err != nil {
+		log.Printf("Ping request failed: %v", err)
+	} else {
+		log.Printf("Ping: %v", pr)
+	}
+
+	sr, err := consumer.Sync(&types.SyncRequest{})
+	if err != nil {
+		log.Printf("Sync request failed: %v", err)
+	} else {
+		log.Printf("Sync: %v", sr)
+	}
+
+	cr, err := consumer.Call(&types.CallRequest{})
+	if err != nil {
+		log.Printf("Call request failed: %v", err)
+	} else {
+		log.Printf("Call: %v", cr)
+	}
+
+	gr, err := consumer.GetProgram(&types.GetProgramRequest{})
+	if err != nil {
+		log.Printf("GetProgram request failed: %v", err)
+	} else {
+		log.Printf("GetProgram: %v", gr)
+	}
 }
