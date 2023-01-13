c3e134ad7bff314e42cf29cb058f8657f0bd6661
kino-ma <ma@kino.ma>
2023-01-13 15:50:14 +0900

    feat: add RunApp

--- core/apps/apps.go
+++ core/apps/apps.go
@@ -1,7 +1,20 @@
 package apps
 
-import "local-fog/core/types"
+import (
+	"fmt"
+	"local-fog/core/types"
+)
 
 var Applications map[types.AppId]types.AppFunction = map[types.AppId]types.AppFunction{
 	1: Hello,
+}
+
+func RunApp(appId types.AppId, in []byte) ([]byte, error) {
+	f, found := Applications[appId]
+	if !found {
+		err := fmt.Errorf("app with id %v not found", appId)
+		return nil, err
+	}
+
+	return f(in)
 }
