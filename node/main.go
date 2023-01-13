16fdddee47749b5b6b4b1dc0554d53bdd383f437
kino-ma <ma@kino.ma>
2023-01-13 17:15:52 +0900

    feat: discover other nodes first

--- node/main.go
+++ node/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"local-fog/core"
 	"log"
 )
@@ -8,7 +9,16 @@ import (
 func main() {
 	node := &Node{}
 
-	err := RegisterAndServeMdns()
+	neighbors, err := core.Discover(16)
+	if err != nil {
+		err = fmt.Errorf("failed to discover neighbors: %v", err)
+		log.Fatal(err)
+	}
+
+	UpdateNeighbors(neighbors)
+	log.Print(Neighbors)
+
+	err = RegisterAndServeMdns()
 	if err != nil {
 		log.Fatalf("could not start mdns server: %v", err)
 	}
