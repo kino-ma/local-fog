985c8af7ca293049949b75b28d8a7dcc26dfe302
kino-ma <ma@kino.ma>
2023-01-14 12:26:30 +0900

    feat: insert/delete of neighbor

--- node/neighbors.go
+++ node/neighbors.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"local-fog/core/types"
 )
 
 var Neighbors []*types.NodeInfoWrapper
 
+var (
+	ErrNeighborNotFound = fmt.Errorf("neighbor with that id was not found")
+)
+
 func UpdateNeighbors(neighbors []*types.NodeInfoWrapper) {
 	Neighbors = neighbors
+}
+
+func InsertNeighbor(neigh *types.NodeInfoWrapper) {
+	Neighbors = append(Neighbors, neigh)
+}
+
+func DeleteNeighbor(neigh *types.NodeInfoWrapper) error {
+	for i, n := range Neighbors {
+		if n.Id == neigh.Id {
+			Neighbors = append(Neighbors[:i], Neighbors[i+1:]...)
+			return nil
+		}
+	}
+
+	return ErrNeighborNotFound
 }
