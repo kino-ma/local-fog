f6879519912002eb3b041c82dac86e8486690ae9
kino-ma <ma@kino.ma>
2023-01-19 12:26:27 +0900

    test: PatchNeighbors

--- node/neighbors_test.go
+++ node/neighbors_test.go
@@ -123,3 +123,75 @@ func TestChooseMonitorTarget(t *testing.T) {
 		}
 	}()
 }
+
+func TestPatchNeighbors(t *testing.T) {
+	comp := func(s1, s2 []*types.NodeInfoWrapper) bool {
+		if len(s1) != len(s2) {
+			return false
+		}
+
+		for i := range s1 {
+			if types.CompareNode(s1[i], s2[i]) != 0 {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	func() {
+		// no common nodes
+		n1 := &types.NodeInfoWrapper{Id: 1}
+		n2 := &types.NodeInfoWrapper{Id: 2}
+		n3 := &types.NodeInfoWrapper{Id: 3}
+		n4 := &types.NodeInfoWrapper{Id: 4}
+		n5 := &types.NodeInfoWrapper{Id: 1}
+		n6 := &types.NodeInfoWrapper{Id: 1}
+
+		s1 := []*types.NodeInfoWrapper{n1, n2, n3}
+		s2 := []*types.NodeInfoWrapper{n4, n5, n6}
+
+		got := PatchNeighbors(s1, s2)
+		want := []*types.NodeInfoWrapper{n1, n2, n3, n4, n5, n6}
+
+		if !comp(want, got) {
+			t.Errorf("no common nodes: chooseMonitorTarget = %v, want %v", got, want)
+		}
+	}()
+
+	func() {
+		// 1 common node
+		n1 := &types.NodeInfoWrapper{Id: 1}
+		n2 := &types.NodeInfoWrapper{Id: 2}
+		n3 := &types.NodeInfoWrapper{Id: 3}
+		n4 := &types.NodeInfoWrapper{Id: 4}
+		n5 := &types.NodeInfoWrapper{Id: 1}
+
+		s1 := []*types.NodeInfoWrapper{n1, n2, n3}
+		s2 := []*types.NodeInfoWrapper{n2, n4, n5}
+
+		got := PatchNeighbors(s1, s2)
+		want := []*types.NodeInfoWrapper{n1, n2, n3, n4, n5}
+
+		if !comp(want, got) {
+			t.Errorf("1 common node: chooseMonitorTarget = %v, want %v", got, want)
+		}
+	}()
+
+	func() {
+		// all common nodes
+		n1 := &types.NodeInfoWrapper{Id: 1}
+		n2 := &types.NodeInfoWrapper{Id: 2}
+		n3 := &types.NodeInfoWrapper{Id: 3}
+
+		s1 := []*types.NodeInfoWrapper{n1, n2, n3}
+		s2 := []*types.NodeInfoWrapper{n1, n2, n3}
+
+		got := PatchNeighbors(s1, s2)
+		want := []*types.NodeInfoWrapper{n1, n2, n3}
+
+		if !comp(want, got) {
+			t.Errorf("all common nodes: chooseMonitorTarget = %v, want %v", got, want)
+		}
+	}()
+}
