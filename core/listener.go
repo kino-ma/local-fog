b278d847bd11c516fa66ed30b3876d63c4a7544b
kino-ma <ma@kino.ma>
2023-01-22 04:11:06 +0900

    feat: fail to accept 1/3 times

--- core/listener.go
+++ core/listener.go
@@ -1,16 +1,26 @@
 package core
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 // DefectListener is wrapper of net.TCPLister.
 //
 // DefectListener sometimes fails to accept connection.
 type DefectListener struct {
-	net.TCPListener
+	*net.TCPListener
 }
 
+var count = 0
+
 func (l *DefectListener) Accept() (net.Conn, error) {
+	count += 1
 	ll := l.TCPListener
-	ll.Close()
-	return nil, net.ErrClosed
+	if count%3 == 0 {
+		ll.Close()
+		return nil, fmt.Errorf("connection closed by failure")
+	}
+
+	return ll.Accept()
 }
