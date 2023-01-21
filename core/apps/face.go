ba6f8f20222eb4174201f0660265f29d62711c3c
kino-ma <ma@kino.ma>
2023-01-22 08:27:10 +0900

    feat: detect face

--- core/apps/face.go
+++ core/apps/face.go
@@ -1,7 +1,33 @@
 package apps
 
-func RecognizeFace(body []byte) ([]byte, error) {
-	out := []byte("hello, world")
+import (
+	"fmt"
+	"log"
 
+	"gocv.io/x/gocv"
+)
+
+const classifierXmlFile string = "../../data/haarcascade_frontalface_default.xml"
+
+func RecognizeFace(body []byte) ([]byte, error) {
+	c := gocv.NewCascadeClassifier()
+	defer c.Close()
+
+	if !c.Load(classifierXmlFile) {
+		err := fmt.Errorf("failed to load classifier xml file")
+		return nil, err
+	}
+
+	img, err := gocv.IMDecode(body, gocv.IMReadUnchanged)
+	if err != nil {
+		err = fmt.Errorf("failed to decode image file: %w", err)
+		return nil, err
+	}
+
+	rects := c.DetectMultiScale(img)
+	msg := fmt.Sprintf("found %v faces", len(rects))
+	log.Print(msg)
+
+	out := []byte(msg)
 	return out, nil
 }
