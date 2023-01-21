f894009e89a0db77bf69e71d4e34e33a8d9afe99
kino-ma <ma@kino.ma>
2023-01-22 05:37:32 +0900

    feat: roundrobin all hosts

--- client/main.go
+++ client/main.go
@@ -9,44 +9,51 @@ import (
 )
 
 const cloudHostName string = "cloud"
-const testDuration = 10 * time.Minute
-const testInterval = 100 * time.Millisecond
+const testDuration = 5 * time.Second
+const testInterval = 1 * time.Millisecond
 
 func main() {
-	nodes, err := core.Discover(1)
+	nodes, err := core.Discover(16)
 	if err != nil {
 		log.Fatalf("failed to discover: %v", err)
 	}
-	var host string
-
-	if len(nodes) < 1 {
-		host = cloudHostName
-	} else {
-		node := nodes[0]
-		addr := utils.Uint32ToIp((node.AddrV4))
-		log.Printf("discovered: %+v", addr)
-		host = addr.String()
-	}
-
-	consumer, err := core.Connect(host, core.DEFAULT_PORT)
-
-	if err != nil {
-		log.Fatalf("failed to connec to the server: %v", err)
-	}
-
 	timeout := time.After(testDuration)
 	ticker := time.NewTicker(testInterval)
+	l := len(nodes)
+	if l == 0 {
+		l = 1
+	}
 
 loop:
-	for {
+	for i := 0; ; i = (i + 1) % l {
 		select {
 		case <-ticker.C:
-			call(&consumer, &types.CallRequest{
-				AppId: 1,
-				Body:  []byte{},
-			})
+			go func(i int) {
+				host := chooseHost(nodes, i)
+				consumer, err := core.Connect(host, core.DEFAULT_PORT)
+
+				if err != nil {
+					log.Fatalf("failed to connec to the server: %v", err)
+				}
+
+				call(&consumer, &types.CallRequest{
+					AppId: 1,
+					Body:  []byte{},
+				})
+			}(i)
 		case <-timeout:
 			break loop
 		}
 	}
+}
+
+func chooseHost(ns []*types.NodeInfoWrapper, i int) string {
+	if len(ns) < i+1 {
+		return cloudHostName
+	}
+
+	node := ns[i]
+	addr := utils.Uint32ToIp((node.AddrV4))
+	log.Printf("discovered: %+v", addr)
+	return addr.String()
 }
