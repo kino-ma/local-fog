18161ba702ffae8bb3a651b08a5a76a457cd63e8
kino-ma <ma@kino.ma>
2023-01-22 21:23:52 +0900

    docs: combine two boxplots

--- expr/plot.py
+++ expr/plot.py
@@ -5,6 +5,8 @@ import csv
 
 from matplotlib import pyplot as plt
 import numpy as np
+import matplotlib.patches as mpatches
+from matplotlib.path import Path
 
 
 COLUMN_HOST = "host"
@@ -28,15 +30,65 @@ def plot_log(file):
         x = int(r[COLUMN_OVERALL_LATENCY])
         d.append(x)
 
-    fig = plt.figure()
+    fig, ax = plt.subplots(
+        nrows=2,
+        figsize=(3, 4),
+        dpi=160,
+        sharex="col",
+        gridspec_kw={"height_ratios": (1, 2)},
+    )
 
-    ax = fig.add_subplot(2, 1, 1)
-    ax.figure.set_figwidth(10.0)
-    ax.figure.set_figheight(10.0)
-    ax.set_title("latencies")
-    ax.set_ylabel("latency (ms)")
+    # plot two same graph
+    ax[0].boxplot(d)
+    ax[1].boxplot(d, sym="")
+    # print(d)
 
-    ax.boxplot(d)
+    # kuttukeru
+    fig.subplots_adjust(hspace=0.0)
+
+    ax[1].set_ylim(5000, 8000)
+    ax[1].set_yticks(np.arange(5000, 8000, 1000))
+
+    ax[0].set_ylim(1310000, 1370000)
+    ax[0].set_yticks(np.arange(1310000, 1370000 + 1, 20000))
+
+    ax[1].spines["top"].set_visible(False)
+
+    ax[0].spines["bottom"].set_visible(False)
+    ax[0].tick_params(axis="x", which="both", bottom=False, labelbottom=False)
+
+    d1 = 0.02  # X軸のはみだし量
+    d2 = 0.03  # ニョロ波の高さ
+    wn = 21  # ニョロ波の数（奇数値を指定）
+
+    pp = (0, d2, 0, -d2)
+    px = np.linspace(-d1, 1 + d1, wn)
+    py = np.array([1 + pp[i % 4] for i in range(0, wn)])
+    p = Path(list(zip(px, py)), [Path.MOVETO] + [Path.CURVE3] * (wn - 1))
+
+    line1 = mpatches.PathPatch(
+        p,
+        lw=4,
+        edgecolor="black",
+        facecolor="None",
+        clip_on=False,
+        transform=ax[1].transAxes,
+        zorder=10,
+    )
+
+    line2 = mpatches.PathPatch(
+        p,
+        lw=3,
+        edgecolor="white",
+        facecolor="None",
+        clip_on=False,
+        transform=ax[1].transAxes,
+        zorder=10,
+        capstyle="round",
+    )
+
+    a = ax[1].add_patch(line1)
+    a = ax[1].add_patch(line2)
 
     plt.tight_layout()
     plt.show()
