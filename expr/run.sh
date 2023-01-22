3f03d4e1c318ea03327ac8114b7b83a5bbcc8c3b
kino-ma <ma@kino.ma>
2023-01-23 00:02:35 +0900

    feat: save logs for each targets

--- expr/run.sh
+++ expr/run.sh
@@ -2,11 +2,29 @@
 
 echo 'starting experiment...'
 
-stdbuf -oL docker stats --format 'table {{.Name}},{{.CPUPerc}},{{.MemPerc}},{{.NetIO}}' > /tmp/stats.log &
+program=$0
+
+usage() {
+    echo "$program identifier"
+}
+
+identifier=$1
+
+if [[ -z $identifier ]]
+then
+    usage
+    exit 1
+fi
+
+log="log/$identifier-log.csv"
+tmp_stats="/tmp/$identifier-stats.log"
+stats="log/$identifier-stats.csv"
+
+stdbuf -oL docker stats --format 'table {{.Name}},{{.CPUPerc}},{{.MemPerc}},{{.NetIO}}' > "$tmp_stats" &
 stats_pid=$!
 
 docker compose up -d node1 node2 node3 cloud
-docker compose run --rm client &>/dev/null
+docker compose run --rm client "$log" &>/dev/null
 
 echo 'client container finished. shutting down...'
 
@@ -14,8 +32,8 @@ docker compose down
 kill $stats_pid
 wait $stats_pid
 
-sed -u 's/\x1b\[[0-9;]*[mGKHFJ]//g' /tmp/stats.log > log/stats.csv
-rm /tmp/stats.log
+sed -u 's/\x1b\[[0-9;]*[mGKHFJ]//g' $tmp_stats > $stats
+rm $tmp_stats
 
 echo 'plot figures...'
 ./expr/plot.py log/log.csv log/stats.csv log/figs/log.png log/figs/stats.png
