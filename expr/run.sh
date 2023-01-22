9dd2c8d13f41581eafe2a2e2891647b3829031d0
kino-ma <ma@kino.ma>
2023-01-22 17:44:59 +0900

    test: wait stats process to finish

--- expr/run.sh
+++ expr/run.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-docker stats --format '{{.Name}},{{.CPUPerc}},{{.MemPerc}},{{.NetIO}}' | sed 's/\x1b\[[0-9;]*[mGKHFJ]//g' > log/stats.csv &
+echo 'starting experiment...'
+
+stdbuf -oL docker stats --format '{{.Name}},{{.CPUPerc}},{{.MemPerc}},{{.NetIO}}' > /tmp/stats.log &
 stats_pid=$!
 
 docker compose up -d node1 node2 node3 cloud
-docker compose run --rm client
+docker compose run --rm client &>/dev/null
+
+echo 'client container finished. shutting down...'
+
 docker compose down
 kill $stats_pid
+wait $stats_pid
+
+sed -u 's/\x1b\[[0-9;]*[mGKHFJ]//g' /tmp/stats.log > log/stats.csv
+rm /tmp/stats.log
+
+echo 'done.'
